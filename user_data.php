7a06139bd2c87b297f55b8ad018002026378bdcd
Oyekunle Dare <115362169+OyekunleDare@users.noreply.github.com>
2022-10-12 21:01:14 +0100

    Update user_data.php

--- user_data.php
+++ user_data.php
@@ -1,5 +1,6 @@
 <?php
 	if(isset($_POST['submit'])){
+		//get the form data into variables
 		$name = $_POST['name'];
 		$email = $_POST['email'];
 		$dob = strtotime($_POST['dob']);
@@ -7,22 +8,23 @@
 		$gender = $_POST['gender'];
 		$country = $_POST['country'];
 		
-		
-		$filename = fopen("userdata.csv", "r+");
+		//give the csv file a heading
+		$csvfile = "./userdata.csv";
+		$filename = fopen($csvfile, "r+");
 		$form = array("Name", "Email", "Date of Birth", "Gender", "Country");
 		fputcsv($filename,$form);
 		fclose($filename);
 		
+		//store the variables in an array and insert into the csv file
 		$formdata = array($name, $email, $newdate, $gender, $country);
-		$file = fopen("userdata.csv", "a");
-				
+		$file = fopen($csvfile, "a");				
 		fputcsv($file,$formdata);
 		fclose($file);
 		
-		$csvfile = "./userdata.csv";
+		//open the csvfile and print it out as an array
 		$data = fopen($csvfile, "r");
 		$user_data = fread($data, filesize($csvfile));
-		print_r($user_data . "\n");
+		print_r($user_data);
 		fclose($data);
 	}
 ?>
